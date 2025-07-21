78fdc8509efdb3eeb6d883cc02472f495d9f8238
Noobs440 <arieldoubissi330@gmail.com>
2025-07-22 01:00:54 +0200

    Modification du web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Usecases\Authcontroller;
-
+use Illuminate\Support\Facades\DB;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,13 @@ use App\Http\Controllers\Usecases\Authcontroller;
 
 
 Route::get('user/verify/{token}', [Authcontroller::class, 'verifyAccount'])->name('user.verify');
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Connexion OK avec la base MySQL ✅';
+    } catch (\Exception $e) {
+        return 'Erreur connexion DB ❌ : ' . $e->getMessage();
+    }
+});
+
